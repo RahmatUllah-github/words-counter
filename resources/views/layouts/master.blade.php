0374d265906950c2040777491f6af9b3ede81cce
Rahmat <rahmatul368@gmail.com>
2023-06-24 17:09:30 +0500

    logo added in meta data

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,13 +8,13 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="msapplication-TileColor" content="#b6b4ed" />
-    <meta name="msapplication-TileImage" content="{{ asset('images/logos/logo.png') }}" />
+    <meta name="msapplication-TileImage" content="{{ asset('images/logos/thewordscounter-logo.png') }}" />
 
     @yield('meta')
 
     <meta name="robots" content="index, follow" />
 
-    <link rel="icon" type="image/x-icon" href="{{ asset('images/logos/favicon-32x32.png') }}">
+    <link rel="icon" type="image/x-icon" href="{{ asset('images/logos/thewordscounter-logo.png') }}">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
